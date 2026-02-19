0a64e323cdb9edbd7c959c8db9ecd37d29e5b2cf
Divya M D <divyamd809@gmail.com>
2026-02-19 12:44:02 +0530

    deleted and added something

--- index.py
+++ index.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Example log data (Replace with real data later)
+# Sample SOC logs
 logs = [
     {"ip": "192.168.1.10", "attack": "Brute Force", "severity": "High"},
     {"ip": "10.0.0.5", "attack": "SQL Injection", "severity": "Critical"},
-    {"ip": "172.16.0.8", "attack": "XSS", "severity": "Medium"}
+    {"ip": "172.16.0.8", "attack": "XSS", "severity": "Medium"},
+    {"ip": "192.168.5.2", "attack": "DDoS", "severity": "Critical"}
 ]
 
 @app.route("/")
